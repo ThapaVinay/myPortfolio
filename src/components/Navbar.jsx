9f74aac1767b4e6f57692fcef4083a88f8f3c20b
lonewolf <thechampthapa@gmail.com>
2023-08-09 13:20:56 +0530

    navbar basic structure done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets"
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <nav className="sm:px-15 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-primary">
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto' >
+        <Link to="/" className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}>
+          <img src={logo} alt="mylogo" className='w-9 h-9 object-contain' />
+
+          <p className="text-white text-[18px] font-bold cursor-pointer"> Vinay <span className='sm:block hidden'>| Full Stack Developer</span></p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+
+          {navLinks.map((link) => (
+            <li key={link.id}
+              className={`${active === link.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+
+          ))}
+
+          <div className=" flex flex-1 justify-end items-center">
+            <img src={toggle ? close:menu} alt="menu"
+              className='w-[24px] h-[24px] object-contain cursor-pointer'
+              onClick = {() => setToggle(!toggle)}
+            />
+            
+          </div>
+
+        </ul>
+      </div>
+    </nav>
+
   )
 }
 
